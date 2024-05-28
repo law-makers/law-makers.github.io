972e405024ea25bcd23bd140db55f1762c49cabe
Zulfikar <155657912+muhammad-zulfikar@users.noreply.github.com>
2024-05-28 17:23:43 +0000

    updates

--- build.sh
+++ build.sh
@@ -8,4 +8,17 @@ prettier -w .
 
 cpu_cores="$(nproc)"
 
+echo "Minifying everything we can"
+find ./public/ -type f \( \
+  -name "*.html" \
+  -o -name '*.js' \
+  -o -name '*.css' \
+  -o -name '*.svg' \
+  -o -name "*.xml" \
+  -o -name "*.json" \
+  -o -name "*.html" \
+  \) \
+  -and ! -name "*.min*" -print0 |
+  xargs -0 -n1 -P"${cpu_cores}" -I '{}' sh -c 'minify -o "{}" "{}"'
+
 cp CNAME public/
